168f54e0aabe0b1cdc1c1032f3a2fded62a66527
Joonas Taivalmaa <joonas.taivalmaa@metropolia.fi>
2020-05-20 22:06:25 +0300

    Added --latex option for printing output in latex table format

--- z-ring.py
+++ z-ring.py
@@ -1,42 +1,44 @@
 import argparse
 import numpy as np
 
-def sumring(mod):
+def sumring(mod, lim, end):
     ls = np.arange(0, mod)
     result = ''
     print("Sum table for modulo" + str(mod))
     for i in range(mod):
         for u in range(mod):
             if u == mod -1:
-                result += str((i+u) % mod) + 2*'\n'
+                result += str((i+u) % mod) + end
             else:
-                result += str((i+u)%mod) + "\t"
+                result += str((i+u)%mod) + lim
 
     print(result)
 
-def multiplyring(mod):
+def multiplyring(mod, lim, end):
     ls = np.arange(0, mod)
     result = ''
     print("Multiplication table for modulo" + str(mod))
     for i in range(mod):
         for u in range(mod):
             if u == mod -1:
-                result += str((i*u) % mod) + 2*'\n'
+                result += str((i*u) % mod) + end
             else:
-                result += str((i*+u)%mod) + "\t"
+                result += str((i*+u)%mod) + lim
 
     print(result)
 
-""" Debugging
-mod = 5
-sumring(mod)
-multiplyring(mod)
-"""
 parser = argparse.ArgumentParser(description='Z-ring calculator')
 parser.add_argument('mod', type=int, help="ring modulo")
+parser.add_argument("-l", "--latex", help="print output in latex table format", action="store_true")
 args = parser.parse_args()
+
 def main():
-    sumring(args.mod)
-    multiplyring(args.mod)
+    lim = "\t"
+    end = "\n"
+    if args.latex:
+        lim = " & "
+        end = " \\\\\n"
+    sumring(args.mod, lim, end)
+    multiplyring(args.mod, lim, end)
 
 main()
